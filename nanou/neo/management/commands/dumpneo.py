1b905b5e2544176b9a4da119dd7e8de787226c65
Max Bothe <mathebox@gmail.com>
2016-10-30 18:30:49 +0100

    feat(neo): add export and import management command
    - dumpneo
      - dumps all neo models in registered apps
    - loadneo
      - loads all models in fixtures file into neo db
      - requires absolute paths

---
+++ nanou/neo/management/commands/dumpneo.py
@@ -0,0 +1,30 @@
+from importlib import import_module
+import inspect
+import json
+
+from django.apps import apps
+from django.apps.config import MODELS_MODULE_NAME
+from django.core.management.base import BaseCommand
+from django.utils.module_loading import module_has_submodule
+
+from neo.models import NeoModel
+
+
+class Command(BaseCommand):
+    help = 'Dumps all neo4j models'
+
+    def handle(self, *args, **options):
+        export = list()
+        for app_name, app_config in apps.app_configs.items():
+            if module_has_submodule(app_config.module, MODELS_MODULE_NAME):
+                models_module_name = '{}.{}'.format(app_config.name, MODELS_MODULE_NAME)
+                models_module = import_module(models_module_name)
+                for member_name, member in inspect.getmembers(models_module, inspect.isclass):
+                    if issubclass(member, NeoModel) and member != NeoModel:
+                        for obj in member.all():
+                            export.append({
+                                'model': '{}.{}'.format(member.__module__, member_name),
+                                'id': obj.id,
+                                'fields': obj.value_dict()
+                            })
+        self.stdout.write(json.dumps(export, indent=4))
---
+++ nanou/neo/management/commands/loadneo.py
@@ -0,0 +1,69 @@
+import json
+
+from django.core.management.base import BaseCommand
+from py2neo.ogm import Property, Related
+
+from neo.utils import NeoGraph
+
+
+class Command(BaseCommand):
+    help = 'Loads given neo4j models'
+
+    def add_arguments(self, parser):
+        parser.add_argument('args', metavar='fixture', nargs='+', help='Fixture labels.')
+
+    def _model_class(self, entry):
+        module_name, _, class_name = entry['model'].rpartition('.')
+        module = __import__(module_name, fromlist='.')
+        return getattr(module, class_name)
+
+    def _properties(self, model_class):
+        return (
+            prop_name
+            for prop_name, prop in model_class.__dict__.items()
+            if (not(prop_name.startswith('__') and prop_name.endswith('__'))
+                and isinstance(prop, Property))
+        )
+
+    def _relationships(self, model_class):
+        return (
+            rel_name
+            for rel_name, rel in model_class.__dict__.items()
+            if (not(rel_name.startswith('__') and rel_name.endswith('__'))
+                and isinstance(rel, Related))
+        )
+
+    def handle(self, *fixture_paths, **options):
+        for path in fixture_paths:
+            object_map = dict()
+            relationship_count = 0
+            with open(path, 'r') as fixture_file:
+                data = json.load(fixture_file)
+
+                # create objects with properties
+                for entry in data:
+                    model_class = self._model_class(entry)
+                    obj = model_class()
+                    for prop_name in self._properties(model_class):
+                        setattr(obj, prop_name, entry['fields'][prop_name])
+                    object_map[entry['id']] = obj
+
+                # create relationship between objects
+                for entry in data:
+                    model_class = self._model_class(entry)
+                    obj = object_map[entry['id']]
+                    for prop_name in self._relationships(model_class):
+                        related_objects = getattr(obj, prop_name)
+                        for related_id in entry['fields'][prop_name]:
+                            rel_obj = object_map[related_id]
+                            related_objects.add(rel_obj)
+                            relationship_count += 1
+
+            with NeoGraph() as graph:
+                for obj in object_map.values():
+                    graph.create(obj)
+            self.stdout.write('{path}: {obj_count} objects and {rel_count} relationships created'.format(**{
+                'path': path,
+                'obj_count': len(object_map),
+                'rel_count': relationship_count/2,
+            }))
