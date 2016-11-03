9d792e01de0fe3742dcc64e5a2e59452a22b0036
Max Bothe <mathebox@gmail.com>
2016-11-03 11:30:12 +0100

    test: add tests for landing page

---
+++ nanou/nanou/tests/test_views.py
@@ -0,0 +1,41 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class NanouViewCorrcetPermissionsMixin(object):
+    """Mixins for user testing the views is logged if the user has the required permissions."""
+    csrf_client = Client(enforce_csrf_checks=True)
+
+    def test_get_landing_page(self):
+        response = self.client.get(reverse('landingpage'))
+        self.assertEqual(response.status_code, 200)
+
+
+class NanouViewWrongPermissionMixin(object):
+    """Mixins for user testing the views is logged if the user lacks the required permissions."""
+
+    def test_get_landing_page(self):
+        url = reverse('landingpage')
+        response = self.client.get(url)
+        self.assertRedirects(response, reverse('login') + '?next=' + url)
+
+
+class NanouViewManagerTests(TestCase, NanouViewCorrcetPermissionsMixin):
+    """User testing the views is logged in as manager and therefore has the required permissions."""
+    fixtures = ['users_testdata']
+
+    def setUp(self):
+        self.client.login(username='manager', password='admin')
+
+
+class NanouViewSocialUserTests(TestCase, NanouViewWrongPermissionMixin):
+    """User testing the views is logged in as social user and therefore lacking the required permissions."""
+    fixtures = ['users_testdata']
+
+    def setUp(self):
+        self.client.login(username='socialuser', password='admin')
+
+
+class NanouViewNoPermissionTests(TestCase, NanouViewWrongPermissionMixin):
+    """User testing the views is not logged and therefore lacking the required permissions."""
+    fixtures = ['users_testdata']
--- nanou/nanou/views.py
+++ nanou/nanou/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
+from groups.models import Group
+from socialusers.models import SocialUser
+from videos.models import Video
+
 
 @permission_required('nanou.manage_curriculum')
 def landingpage(request):
-    return render(request, 'landingpage.html')
+    return render(request, 'landingpage.html', {
+        'video_count': len(list(Video.all())),
+        'group_count': len(list(Group.all())),
+        'socialuser_count': len(list(SocialUser.all())),
+    })
