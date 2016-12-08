5553c1e3454e198874d740f86ed4584e5300c0aa
Max Bothe <mathebox@gmail.com>
2016-12-08 14:24:57 +0100

    fix(api): correct serializer for preferences

--- nanou/api/serializers.py
+++ nanou/api/serializers.py
@@ -11,7 +11,7 @@ class PreferenceSerializer(serializers.Serializer):
     weight = serializers.SerializerMethodField()
 
     def get_weight(self, obj):
-        socialuser = self.context.pop('socialuser', None)
+        socialuser = self.context.get('socialuser')
         if socialuser:
             return float(socialuser.preferences.get(obj, 'weight', default=1.0))
         return 1.0
