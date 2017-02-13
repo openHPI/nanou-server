134fd4e62bc6f8e11a85d43487727341872df116
Max Bothe <mathebox@gmail.com>
2017-02-13 13:25:03 +0100

    feat(api): set default progress of videos to 0

--- nanou/socialusers/properties.py
+++ nanou/socialusers/properties.py
@@ -3,7 +3,7 @@ from datetime import datetime
 WATCHED_DEFAULT_PROPS = {
     'date': datetime.min.isoformat(),
     'rating': -1,
-    'progress': -1,
+    'progress': 0,
 }
 
 PREFERENCE_DEFAULT_PROPS = {
